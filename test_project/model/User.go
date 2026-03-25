d0684f02c868a5bad62bce6af5c79281798ae673
user <rachidnovikov34@gmail.com>
2026-03-25 21:07:58 +0300

    Crud: Разработал User структуру.

--- test_project/model/User.go
+++ test_project/model/User.go
@@ -4,4 +4,10 @@ type User struct {
 	/*
 		В этом файле нужно будет прописать модель, то есть как у тебя выглядит твоя таблица в бд.
 	*/
+	id         int    `db:"id"`
+	name       string `db:"name"`
+	email      string `db:"email"`
+	password   string `db:"password"`
+	age        int    `db:"age"`
+	department string `db:"department"`
 }
