9c307ce45e5b744121e43018a657c8c83f72d8a4
user <rachidnovikov34@gmail.com>
2026-03-25 21:39:17 +0300

    InsertUser: сделал функцию, которая вставляет данные пользователя в БД.

--- test_project/repository/WorkWithBd.go
+++ test_project/repository/WorkWithBd.go
@@ -1,5 +1,18 @@
 package repository
 
+import (
+	"database/sql"
+)
+
 /*
-	В этом файле нужно будет работать с бд, доставать, сохранять и удалять пользователей
+В этом файле нужно будет работать с бд, доставать, сохранять и удалять пользователей
 */
+// Функция вставки нового пользователя.
+func InsertUser(db *sql.DB, id, name, email, password, department string, age int) error {
+	query := `INSERT INTO users (id, name, email, password, department, age) VALUES ($1, $2, $3, $4, $5, $6)`
+	_, err := db.Exec(query, id, name, email, password, department, age)
+	if err != nil {
+		return err
+	}
+	return nil
+}
