03d1939ab1567d3264044efd566ab89d799c5975
user <rachidnovikov34@gmail.com>
2026-03-25 23:59:30 +0300

    Фронт

--- test_project/controller/HelloWorldController.go
+++ test_project/controller/HelloWorldController.go
@@ -10,5 +10,5 @@ func SetupRoutes(r *gin.Engine, webHandler *webHandlers.WebHandler) {
 
 	// Веб-интерфейс
 	r.GET("/", webHandler.Index)
-	r.GET("add-user", webHandler.CreateUser)
+	r.POST("/add-user", webHandler.CreateUser)
 }
